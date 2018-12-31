52aeffd02ba55df8d127899147fd9978e37d3ad8
Nikhil <nkhlkamboj@gmail.com>
2018-12-31 17:06:19 +0800

    Create BallInContainers.java

---
+++ BallInContainers.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class BallInContainers {
+public static void main(String args[])
+{
+    int row = 3;
+    int col = 3;
+    int matrix[][] = new int[row][col];
+
+    System.out.print("Hello");
+
+    for (int i=0;i<row;i++)
+    {
+        for (int j=0;j<col;j++)
+        {
+            if(i==j)
+                continue;
+            matrix[i][j]
+
+        }
+
+    }
+
+}
+
+
+
+
+
+}
